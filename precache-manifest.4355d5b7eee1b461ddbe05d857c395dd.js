fdce83d123daa5ef421a30f791d3732db1000526
Jeff Anderson-Lester <viperhq@hotmail.com>
2022-01-14 17:48:29 -0600

    Updates

---
+++ precache-manifest.4355d5b7eee1b461ddbe05d857c395dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ea66ff776c67c75712de24dc4d5598",
+    "url": "/My-React-Portfolio/index.html"
+  },
+  {
+    "revision": "26225129ad76779e9f2e",
+    "url": "/My-React-Portfolio/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "75ea2b6e7f369e82b85d",
+    "url": "/My-React-Portfolio/static/css/main.ca7329ed.chunk.css"
+  },
+  {
+    "revision": "26225129ad76779e9f2e",
+    "url": "/My-React-Portfolio/static/js/2.2db0dbb0.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/My-React-Portfolio/static/js/2.2db0dbb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc5e4c1c795a18a4978",
+    "url": "/My-React-Portfolio/static/js/3.1f463ed7.chunk.js"
+  },
+  {
+    "revision": "75ea2b6e7f369e82b85d",
+    "url": "/My-React-Portfolio/static/js/main.38cdd5f5.chunk.js"
+  },
+  {
+    "revision": "7ed02d75dc77cc947980",
+    "url": "/My-React-Portfolio/static/js/runtime-main.2849470c.js"
+  },
+  {
+    "revision": "dda97a1a5e50e1a018339b45c0ee1e2f",
+    "url": "/My-React-Portfolio/static/media/Profile1.dda97a1a.jpg"
+  },
+  {
+    "revision": "cffce4fba99a0a0f520c2b205a21a4c1",
+    "url": "/My-React-Portfolio/static/media/autox001.cffce4fb.jpg"
+  }
+]);
